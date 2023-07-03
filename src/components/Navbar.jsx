3f1f20ac4840620d19fcded6efbd4cb6bb4c213b
Brandon Atkinson <brandon.atkinson@fdmgroup.com>
2023-07-03 15:08:21 +0100

    navbar commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,17 +19,17 @@ const Navbar = () => {
             <div className="menu-icon" onClick={toggleNavbar}>
                 <AiOutlineMenu />
             </div>
-            {expanded && (
-                <ul className="navbar-links">
-                    <li onClick={closeNavbar}><Link to='/'> Home </Link> </li>
-                    <li onClick={closeNavbar} ><Link to='/Personal_Profile'> Personal Profile </Link> </li>
-                    <li onClick={closeNavbar}><Link to='/Portfolio'> Portfolio </Link> </li>
-                    <li onClick={closeNavbar}><Link to='/Education'> Education </Link></li>
-                    <li onClick={closeNavbar}><Link to='/Experience'> Experience </Link></li>
-                    <li onClick={closeNavbar}><Link to='/Skills'> Skills </Link></li>
-                    <li onClick={closeNavbar}><Link to='/Hobbies_And_Interests'> Hobbies and Interests </Link></li>
+            
+                <ul className={ (expanded == true) ? 'navbar-links' :'navbar-links-expanded'} >
+                    <li onClick={toggleNavbar}><Link to='/'> Home </Link> </li>
+                    <li onClick={toggleNavbar} ><Link to='/Personal_Profile'> Personal Profile </Link> </li>
+                    <li onClick={toggleNavbar}><Link to='/Portfolio'> Portfolio </Link> </li>
+                    <li onClick={toggleNavbar}><Link to='/Education'> Education </Link></li>
+                    <li onClick={toggleNavbar}><Link to='/Experience'> Experience </Link></li>
+                    <li onClick={toggleNavbar}><Link to='/Skills'> Skills </Link></li>
+                    <li onClick={toggleNavbar}><Link to='/Hobbies_And_Interests'> Hobbies and Interests </Link></li>
                 </ul>
-            )}
+            
         </nav>
     );
 };
